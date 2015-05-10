7facb7a209b0bf13c4184b50e444a5f553a0ff95
Edd Barrett <vext01@gmail.com>
2015-05-10 19:26:24 +0100

    Try to be resilient to error.

--- user.lua
+++ user.lua
@@ -1,18 +1,40 @@
 conf = require("conf")
 
+num_acks = 0
+
 function ack(conn)
 	print("ack")
+	num_acks = num_acks + 1
+
+	-- the last one?
+	if num_acks == conf.num_sensors then
+		again(conn, false)
+	end
 end
 
-function again(conn)
+function too_long()
+	print("This is taking too long, reboot!")
+	again(nil, true)
+end
+
+function again(conn, fail)
 	-- blink the ok led
+	local led = conf.ack_led
+	if fail then
+		led = conf.power_led
+	end
+
 	for i = 10, 1, -1 do
 		gpio.write(conf.ack_led, gpio.HIGH)
 		tmr.delay(0.03 * 1000 * 1000)
 		gpio.write(conf.ack_led, gpio.LOW)
 		tmr.delay(0.03 * 1000 * 1000)
 	end
-	conn:close()
+
+	if conn ~= nil then
+		conn:close()
+	end
+
 	print(string.format("Going to sleep for %d secs...", conf.read_intvl))
 	node.dsleep(conf.read_intvl * 1000 * 1000)
 end
@@ -22,12 +44,10 @@ function rcv(conn)
 	for en_pin, topic in pairs(conf.sensors) do
 		gpio.write(en_pin, gpio.HIGH)
 		local val = adc.read(0)
-		print("adc val was %d", val)
 		gpio.write(en_pin, gpio.LOW)
 		print(string.format("pin=%s, topic=%s, val=%d", en_pin, topic, val))
-		mq:publish(topic, tostring(val), conf.mqtt_qos, conf.mqtt_retain, ack)
+		conn:publish(topic, tostring(val), conf.mqtt_qos, conf.mqtt_retain, ack)
 	end
-	again(conn)
 end
 
 -- main
@@ -35,6 +55,9 @@ for en_pin, topic in pairs(conf.sensors) do
 	gpio.mode(en_pin, gpio.OUTPUT)
 	gpio.write(en_pin, gpio.LOW)
 end
+
+-- this shouldn't take long at all
+tmr.alarm(0, 10 * 1000, 0, too_long)
 
 print(string.format("Connecting to %s:%d", conf.mqtt_broker, conf.mqtt_port))
 mq = mqtt.Client("clientid", conf.mqtt_keepalive, conf.mqtt_user, conf.mqtt_pass)
